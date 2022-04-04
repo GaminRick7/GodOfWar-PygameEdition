3c6c3f0980a7ab84666d49b5666af73dc299a681
Raihaan Sandhu <raihaansandhu@gmail.com>
2022-04-04 17:56:24 -0400

    commit

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("hello world!")
-print("lol")
+import pygame, sys
+
+pygame.init()
+screen_width = 1200
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill('red')
+    pygame.display.update()
+    clock.tick(60)
