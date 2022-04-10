8febafa6b2ea9932978ff1d0ad18fc8d2c4314e5
Raihaan Sandhu <raihaansandhu@gmail.com>
2022-04-10 00:16:00 -0400

    commit

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-import pygame, sys
+import pygame
+import sys
+import os
 
-pygame.init()
-screen_width = 1200
-screen_height = 700
-screen = pygame.display.set_mode((screen_width, screen_height))
+############## VARIABLES ###############
+
+worldx = 960
+worldy = 720
+fps = 40  # frame rate
+ani = 4   # animation cycles
+main = True
+
+BLUE = (25, 25, 200)
+BLACK = (23, 23, 23)
+WHITE = (254, 254, 254)
+
+
+############### OBJECTS ##################
+
+# put Python classes and functions here
+
+
+############### INITIAL SETUP #############
+
 clock = pygame.time.Clock()
+pygame.init()
+world = pygame.display.set_mode([worldx, worldy])
+backdrop = pygame.image.load(os.path.join('images', 'stage.png'))
+backdropbox = world.get_rect()
 
-while True:
+
+################ MAIN ####################
+
+while main:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
-    screen.fill('red')
-    pygame.display.update()
-    clock.tick(60)
+            try:
+                sys.exit()
+            finally:
+                main = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord('q'):
+                pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                main = False
+    world.blit(backdrop, backdropbox)
+    pygame.display.flip()
+    clock.tick(fps)
