ed97dcea0c07d36f61e1d632fabbe991ae645c71
Raihaan Sandhu <raihaansandhu@gmail.com>
2022-04-19 18:12:23 -0400

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import os
+from player import Player
 
 from spritesheet import Spritesheet
 
@@ -8,7 +9,7 @@ from spritesheet import Spritesheet
 #
 
 worldx = 960
-worldy = 720
+worldy = 480
 fps = 40  # frame rate
 ani = 4   # animation cycles
 main = True
@@ -20,17 +21,6 @@ ALPHA = (0, 255, 0)
 
 ############### OBJECTS ##################
 
-# put Python classes and functions here
-
-class Player(pygame.sprite.Sprite):
-    """
-    Spawn a player
-    """
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.LEFT_KEY, self.RIGHT_KEY, self.FACING_LEFT = False, False, False
-
 
 
 ############### INITIAL SETUP #############
@@ -41,6 +31,12 @@ world = pygame.display.set_mode([worldx, worldy])
 backdrop = pygame.image.load(os.path.join('images', 'mountain.png'))
 backdropbox = world.get_rect()
 
+player = Player()  # spawn player
+player.rect.x = 0  # go to x
+player.rect.y = 0  # go to y
+player_list = pygame.sprite.Group()
+player_list.add(player)
+steps = 2
 
 ################ MAIN ####################
 
@@ -48,18 +44,27 @@ while main:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            try:
-                sys.exit()
-            finally:
-                main = False
+            sys.exit()
 
         if event.type == pygame.KEYDOWN:
             if event.key == ord('q'):
                 pygame.quit()
-            try:
-                sys.exit()
-            finally:
-                main = False
-    world.blit(backdrop, backdropbox)
+                try:
+                    sys.exit()
+                finally:
+                    main = False
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                    player.control(-steps, 0)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player.control(steps, 0)
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                print(0, 10)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                player.control(steps, 0)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player.control(-steps, 0)
     pygame.display.flip()
     clock.tick(fps)
+    player.update()
+    player_list.draw(world)
---
+++ player.py
@@ -0,0 +1,64 @@
+import pygame
+import sys
+import os
+
+ani = 4
+ALPHA = (0, 255, 0) 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.movex = 0
+        self.movey = 0
+        self.frame = 0
+        self.walk_images = []
+        self.jump_images = []
+        for i in range(0, 7):
+            walk_image = pygame.image.load(os.path.join('images', f'walk00{str(i)}.png')).convert()
+            walk_image.convert_alpha()  # optimise alpha
+            walk_image.set_colorkey(ALPHA)  # set alpha
+            self.walk_images.append(walk_image)
+            self.image = self.walk_images[0]
+            self.rect = self.image.get_rect()
+
+        for i in range(0, 7):
+            jump_image = pygame.image.load(os.path.join('images', 'jump', f'tile00{str(i)}.png')).convert()
+            jump_image.convert_alpha()  # optimise alpha
+            jump_image.set_colorkey(ALPHA)  # set alpha
+            self.jump_images.append(jump_image)
+
+
+    def control(self, x, y):
+        """
+        control player movement
+        """
+        self.movex += x
+        self.movey += y
+
+    def update(self):
+        """
+        Update sprite position
+        """
+
+        self.rect.x = self.rect.x + self.movex
+        self.rect.y = self.rect.y + self.movey
+
+        # moving left
+        if self.movex < 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = pygame.transform.flip(self.walk_images[self.frame // ani], True, False)
+
+        # moving right
+        if self.movex > 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = self.walk_images[self.frame//ani]
+
+        if self.movey > 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = self.jump_images[self.frame//ani]
