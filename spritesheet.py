57159e503009979187ab79b7a346be1a6891856c
GaminRick7 <63803872+GaminRick7@users.noreply.github.com>
2022-04-19 08:56:37 -0400

    Update spritesheet.py

--- spritesheet.py
+++ spritesheet.py
@@ -1,12 +1,12 @@
 import pygame
 
 class Spritesheet:
-    def __init__(self, filename):
-        self.filename = filename
-        self.sprite_sheet = pygame.image.load(filename).convert()
-    
-    def get_sprite(self, x, y, w, h):
-        sprite = pygame.Surface((w,h))
-        sprite.set_colorkey((0,0,0))
-        sprite.blit(self.sprite_sheet,(0,0), (x,y,w,h,))
-        return sprite
+    def __init__(self, image):
+      self.sheet = image
+
+    def get_image(self, frame, width, height, scale, colour):
+      image = pygame.Surface((width,height)).convert_alpha()
+      image.blit(self.sheet, (0,0), ((frame*width), 0, width, height))
+      image = pygame.transform.scale(image, (width*scale, height * scale))
+      image.set_colorkey(colour)
+      return image
